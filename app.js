32f48334968c95ef175a103107d0ebdc2a7aa33a
Anthony Albertorio <anthony.albertorio@gmail.com>
2016-08-10 16:42:21 -0400

    fixed comments in app.js file.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // get profile
 var profile = require("./profile.js");
 
-// get zipcode arguments inside console.
+// get users as arguments inside console.
 var users = process.argv.slice(2);
 
 // allows you to pass any number of user
 // usually forEach is not supported in all browsers
 // however, since node is based on Chrome's V8 engine, no need to worry
 // works since users.forEach passes 1 parameter and the get method takes in 1 paramenter
-zipCode.forEach(profile.get);
+users.forEach(profile.get);
 
 
 /*
